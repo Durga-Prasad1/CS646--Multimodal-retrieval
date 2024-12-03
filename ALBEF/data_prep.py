6546ffd782efbad2d783aa94f7bc5a11ee486b54
Durga Prasad <durgamaram98@gmail.com>
2024-12-03 17:13:38 -0500

    fixing data prep

--- ALBEF/data_prep.py
+++ ALBEF/data_prep.py
@@ -10,11 +10,14 @@ import argparse
 
 def prep_test_data(args):
     data = []
-    df = pd.read_csv(args.data_path).fillna('')
-    samples_needed = args.sample_count if args.sample_count > 0 else len(df)
-    processed_row_indices = []
+    df = None 
+    if args.sample_count > 0 : 
+        df = pd.read_csv(args.data_path,nrows = args.sample_count).fillna('')
+    else:
+        df = pd.read_csv(args.data_path).fillna('')
+
     for index,row in df.iterrows():
-        product_image_fetched = False
+        
         if row['product_locale'] != 'us' : continue
 
         img_id = str(row['product_id'])+'_'+str(0)
@@ -25,58 +28,58 @@ def prep_test_data(args):
             image = Image.open(io.BytesIO(ast.literal_eval(row['image0'])))
             image.save(img_path)
             data.append(data_info)
-            processed_row_indices.append(index)
-            samples_needed -= 1
 
         elif row['image_urls'] == '':
             data_info['image'] = ""
             data.append(data_info)
-            processed_row_indices.append(index)
-            samples_needed -= 1
+
 
         else:
-            print('fetching_image')
-            for img_index,img_url in enumerate(row['image_urls'].split('[P_IMG]')[1:]):
-                if product_image_fetched : continue
-                retries = 3
-                for _ in range(retries):
-                    try:
-                        response = requests.get(img_url.strip())
-                        if response.status_code == 200:
-                            product_image_fetched = True
-                            img = Image.open(io.BytesIO(response.content))
-                            img.save(img_path)
-                            data.append(data_info)
-                            processed_row_indices.append(index)
-                            samples_needed -= 1
-                            break
-                        time.sleep(1)  # Wait for 2 seconds before retrying
-                    except:
-                        time.sleep(1)
+            product_image_fetched = False
+            if args.fetch_image:
+                print('fetching_image')
+                for img_index,img_url in enumerate(row['image_urls'].split('[P_IMG]')[1:]):
+                    if product_image_fetched : break
+                    retries = 3
+                    for _ in range(retries):
+                        try:
+                            response = requests.get(img_url.strip())
+                            if response.status_code == 200:
+                                product_image_fetched = True
+                                img = Image.open(io.BytesIO(response.content))
+                                img.save(img_path)
+                                data.append(data_info)
+                                break
+                            time.sleep(1)  # Wait for 2 seconds before retrying
+                        except:
+                            time.sleep(1)
+            if product_image_fetched == False: 
+                 data_info['image'] = ""
+                 data.append(data_info)
 
-        if samples_needed <= 0 : 
-            with open(args.out_path, 'w') as file:
-                file.write(json.dumps(data))
-            
-            data_labels = {}
-            data_df = df.iloc[processed_row_indices]
-            for qid in data_df['query_id'].unique():
-                data_labels[ str(qid)] = {}
-                for i,row in data_df[data_df['query_id'] == qid].iterrows():
-                    data_labels[ str(qid)][row['product_id']] = row['esci_label']
-            
-            with open(args.labels_out_path, 'w') as file:
-                json.dump(data_labels,file)
+        
+    with open(args.out_path, 'w') as file:
+        file.write(json.dumps(data))
+    
+    data_labels = {}
+    for qid in df['query_id'].unique():
+        data_labels[str(qid)] = {}
+        for i,row in df[df['query_id'] == qid].iterrows():
+            data_labels[str(qid)][row['product_id']] = row['esci_label']
+    
+    with open(args.labels_out_path, 'w') as file:
+        json.dump(data_labels,file)
 
-            return
+    return
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', default='../../esci_rand_test_image_5k_good.csv')    
+    parser.add_argument('--data_path', default='../../esci_rand_test_10k.csv')    
     parser.add_argument('--out_path', default='./data/test_data.json')
     parser.add_argument('--labels_out_path', default='./data/test_labels.json')
     parser.add_argument('--img_root_path', default='./data/images')
     parser.add_argument('--sample_count', default = 10,type=int)
+    parser.add_argument('--fetch_image', default=False)
     args = parser.parse_args()
     prep_test_data(args)
